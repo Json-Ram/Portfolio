64c3ffdf169288171ab5553de488b853ec962c81
Json-Ram <jasonram778@gmail.com>
2023-03-16 15:52:24 -0700

    create sections for contact, hero, who, work

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,22 @@ import Hero from "./components/Hero";
 import Who from "./components/Who";
 import Work from "./components/Work";
 import Contact from "./components/Contact";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 100vh;
+  background-color: rebeccapurple;
+`
 
 function App() {
 
   return (
-    <div>
+    <Container>
       <Hero/>
       <Who/>
       <Work/>
       <Contact/>
-    </div>
+    </Container>
   )
 }
 
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import styled from "styled-components";
+
+const Section = styled.div`
+  height: 100vh;
+  background-color: rebeccapurple;
+`
 
 const Work = () => {
   return (
-    <div className="div">Work</div>
+    <Section>Work</Section>
   )
 };
 
