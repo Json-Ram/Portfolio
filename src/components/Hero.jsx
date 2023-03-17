bd4e4e0da66f333d2f97d6af902733d7dd01fbf5
Json-Ram <jasonram778@gmail.com>
2023-03-16 18:11:39 -0700

    add style to hero text, image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -37,19 +37,54 @@ const WhatWeDo = styled.div`
   gap: 10px;
 `;
 
-const Line = styled.img``;
+const Line = styled.img`
+  height: 5px;
+`;
 
-const Subtitle = styled.h2``;
+const Subtitle = styled.h2`
+  color: white;
+`;
 
-const Description = styled.p``;
+const Description = styled.p`
+  font-size: 24px;
+  color: lightgrey;
+`;
 
-const Button = styled.button``;
+const Button = styled.button`
+  background-color: #020024;
+  color: white;
+  font-weight: 500;
+  border: none;
+  border-radius: 5px;
+  height: 50px;
+  width: 100px;
+  cursor: pointer;
+  padding: 10px;
+`;
 
 const Right= styled.div`
   flex: 3;
+  position: relative;
 `;
 
-const Img = styled.img``;
+const Img = styled.img`
+  width: 800px;
+  height: 600px;
+  object-fit: contain;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+  animation: animate 2s infinite ease alternate;
+
+  @keyframes animate {
+    to{
+      transform: translateY(20px);
+    }
+  }
+`;
 
 
 const Hero = () => {
@@ -61,7 +96,7 @@ const Hero = () => {
           <Title>Speed. Violence. Momentum.</Title>
           <WhatWeDo>
             <Line src="./img/line.png"/>
-            <Subtitle></Subtitle>
+            <Subtitle>What I am</Subtitle>
           </WhatWeDo>
           <Description>Lorem ipsum dolor sit amet, consectetur adipiscing elit</Description>
           <Button>Learn More</Button>
