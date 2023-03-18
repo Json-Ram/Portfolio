b06507e729a8dad90f5db0ac3762dde925573b11
Json-Ram <jasonram778@gmail.com>
2023-03-17 23:56:00 -0700

    add minor style changes to work section

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -4,6 +4,21 @@ import styled from "styled-components";
 const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
+  display: flex;
+  justify-content: center;
+`
+const Container = styled.div`
+  width: 1400px;
+  display: flex;
+  justify-content: space-between;
+`
+const Left = styled.div`
+`
+const List = styled.ul`
+`
+const ListItem = styled.li`
+`
+const Right = styled.div`
 `
 
 const Work = () => {
