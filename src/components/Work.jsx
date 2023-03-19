bc44c1dca0686c4d16abc7444a1fd8d9cca81790
Json-Ram <jasonram778@gmail.com>
2023-03-18 20:20:09 -0700

    Add minor style changes to work section

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import styled from "styled-components";
 
+const data = [
+  "Web Design",
+  "Development",
+  "Illustration",
+  "Product Design",
+  "Social Media"
+];
+
 const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
@@ -13,17 +21,36 @@ const Container = styled.div`
   justify-content: space-between;
 `
 const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
 `
 const List = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
 `
 const ListItem = styled.li`
+
 `
 const Right = styled.div`
 `
 
 const Work = () => {
   return (
-    <Section>Work</Section>
+    <Section>
+      <Container>
+        <Left>
+          <List>
+            {data.map((item) => (
+              <ListItem key={item}>{item}</ListItem>
+            ))}
+          </List>
+        </Left>
+        <Right></Right>
+      </Container>
+    </Section>
   )
 };
 
