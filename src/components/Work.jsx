9b32bf5d2d025fe23aa8d475c789a70d45c8d7c0
Json-Ram <jasonram778@gmail.com>
2023-03-18 20:24:49 -0700

    add text styling and size to clickable items

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -14,28 +14,33 @@ const Section = styled.div`
   scroll-snap-align: center;
   display: flex;
   justify-content: center;
-`
+`;
 const Container = styled.div`
   width: 1400px;
   display: flex;
   justify-content: space-between;
-`
+`;
 const Left = styled.div`
   flex: 1;
   display: flex;
   align-items: center;
-`
+`;
 const List = styled.ul`
   list-style: none;
   display: flex;
   flex-direction: column;
   gap: 20px;
-`
+`;
 const ListItem = styled.li`
-
-`
+  font-size: 100px;
+  font-weight: bold;
+  cursor: pointer;
+  color: transparent;
+  -webkit-text-stroke: 1px white;
+`;
 const Right = styled.div`
-`
+  flex: 1;
+`;
 
 const Work = () => {
   return (
