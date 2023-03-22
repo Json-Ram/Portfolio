0e9c3efa011158a5a5799446f7c399848903f5be
Json-Ram <jasonram778@gmail.com>
2023-03-21 23:20:30 -0700

    add style to contact form, button

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,35 +7,57 @@ const Section = styled.div`
 `;
 
 const Container = styled.div`
-  
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  gap: 50px;
 `;
 
 const Left = styled.div`
-  
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
 `;
 
 const Title = styled.h1`
-  
+  font-weight: 200;
 `;
 
 const Form = styled.form`
-  
+  width: 500px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
 `;
 
 const Input = styled.input`
-  
+  padding: 20px;
+  background-color: white;
+  border: none;
+  border-radius: 5px;
 `;
 
 const TextArea = styled.textarea`
-  
+  padding: 20px;
+  border: none;
+  border-radius: 5px;
+  background-color: white;
 `;
 
 const Button = styled.button`
-  
+  background-color: #020024;
+  color: white;
+  border: none;
+  font-weight: bold;
+  cursor: pointer;
+  border-radius: 5px;
+  padding: 20px;
 `;
 
 const Right = styled.div`
-  
+  flex: 1;
 `;
 
 const Contact = () => {
@@ -47,7 +69,7 @@ const Contact = () => {
             <Title>Contact Me</Title>
             <Input placeholder="Name"/>
             <Input placeholder="Email"/>
-            <TextArea placeholder="Write your message"/>
+            <TextArea placeholder="Write your message" rows={10}/>
             <Button>Send</Button>
           </Form>
         </Left>
